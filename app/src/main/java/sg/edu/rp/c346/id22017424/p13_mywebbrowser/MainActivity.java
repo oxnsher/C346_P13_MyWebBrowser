64cbdf8593867cccb5981128b396193a8e512d31
shernice <22017424@myrp.edu.sg>
2023-08-21 11:39:19 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22017424/p13_mywebbrowser/MainActivity.java
@@ -0,0 +1,30 @@
+package sg.edu.rp.c346.id22017424.p13_mywebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.rp.edu.sg/soi/home";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
